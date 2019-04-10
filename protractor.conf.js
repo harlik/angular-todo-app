2ad2b735a00de145483b386fde82f66d0bca0ac9
Olga Svalova <harlozavr@gmail.com>
2019-04-10 10:44:40 +0700

    ste chrome headless via travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,10 +9,7 @@ exports.config = {
     './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome',
-    chromeOptions: {
-      args: ["--headless"]
-  }
+    'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
