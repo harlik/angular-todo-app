85f461374ea88bb5be1da49f42f0c03f0d2153e6
Olga Svalova <harlozavr@gmail.com>
2019-04-10 11:29:12 +0700

    adding the second tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,4 +11,11 @@ describe('todo-app App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Todos');
   });
+
+  it('should have 1 to-do input with required placeholder', () => {
+    page.navigateTo(); 
+    expect(page.getNewTodo().count()).toEqual(1);
+    expect(page.getNewTodoPlaceholder()).toEqual('What needs to be done?');
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,13 @@ export class TodoAppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getNewTodo() {
+    return element.all(by.css('input.new-todo'));
+  }
+
+  getNewTodoPlaceholder() {
+    return element(by.css('input.new-todo')).getAttribute('placeholder');
+  }
+
 }
